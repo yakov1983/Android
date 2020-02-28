09c5797751b026f2541f9baf47544116fe31134d
Yakov <volya_1983@tut.by>
2020-02-28 21:18:32 +0300

    add the second Family

--- Family/app/src/main/java/com/example/family/MainActivity.java
+++ Family/app/src/main/java/com/example/family/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.family;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText login = (EditText) findViewById(R.id.login);
+        final EditText password = (EditText) findViewById(R.id.password);
+        Button loginBtn = findViewById(R.id.loginBtn);
+
+        final TextView errorMsg = findViewById(R.id.errorMsg);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String error = "";
+                if(login.getText().toString().equals("")) {
+                    error = "Заполните поле \"Логин\"\n";
+                }
+
+                if(password.getText().toString().equals("")) {
+                    error += "Заполните поле \"Пароль\"\n";
+                }
+
+                if (!error.equals("")) {
+                    errorMsg.setText(error);
+                    errorMsg.setVisibility(View.VISIBLE);
+                    return;
+                }
+
+                showMenuActivity();
+            }
+        });
+    }
+    public void showMenuActivity() {
+        Intent i = new Intent(this, MenuActivity.class);
+        startActivity(i);
     }
 }
